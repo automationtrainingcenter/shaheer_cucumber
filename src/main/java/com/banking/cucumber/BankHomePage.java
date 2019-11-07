f25b1d7c13dc1c238894c1f8aa7f35c315e3016b
automationtrainingcenter <atcsurya@gmail.com>
2019-11-07 12:22:05 +0530

    BankHomePage class using page object model

--- src/main/java/com/banking/cucumber/BankHomePage.java
+++ src/main/java/com/banking/cucumber/BankHomePage.java
@@ -1,5 +1,50 @@
 package com.banking.cucumber;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BankHomePage {
+	private WebDriver driver;
+	private WebDriverWait wait;
+
+	// username
+	public WebElement username() {
+		WebElement uname = driver.findElement(By.id("txtuId"));
+		return uname;
+	}
+
+	// password
+	public WebElement password() {
+		return driver.findElement(By.id("txtPword"));
+	}
+
+	// login button
+	public WebElement loginButton() {
+		return driver.findElement(By.id("login"));
+	}
+
+	public BankHomePage(WebDriver driver) {
+		this.driver = driver;
+		this.wait = new WebDriverWait(driver, 30);
+	}
+
+	// actions
+	// fill user name
+	public void fillUserName(String username) {
+		wait.until(ExpectedConditions.visibilityOf(this.username())).sendKeys(username);
+	}
+
+	// fill password
+	public void fillPassword(String password) {
+		wait.until(ExpectedConditions.visibilityOf(this.password())).sendKeys(password);
+	}
+
+	// click login button
+	public void clickLogin() {
+		wait.until(ExpectedConditions.elementToBeClickable(this.loginButton())).click();
+	}
 
 }
--- src/test/java/com/banking/cucumber/TestRunner.java
+++ src/test/java/com/banking/cucumber/TestRunner.java
@@ -1,5 +1,29 @@
 package com.banking.cucumber;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestRunner {
+	private WebDriver driver;
+	
+	public void openBrowser() {
+		System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://primusbank.qedgetech.com/home.aspx");
+		driver.manage().window().maximize();
+	}
+	
+	public void login() {
+		BankHomePage bankHomePageObj = new BankHomePage(driver);
+		bankHomePageObj.fillUserName("Admin");
+		bankHomePageObj.fillPassword("Admin");
+		bankHomePageObj.clickLogin();
+	}
+	
+	public static void main(String[] args) {
+		TestRunner runner = new TestRunner();
+		runner.openBrowser();
+		runner.login();
+	}
 
 }
